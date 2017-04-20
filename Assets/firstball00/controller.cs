0871e715ff085c239f2f776cb34db242dd7cc346
cenue <cen.ue86@gmail.com>
2017-04-20 17:30:05 +0800

    ball

---
+++ Assets/firstball00/controller.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controller : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void startGame()
+    {
+        Application.LoadLevel("firstball01");
+    }
+
+    public void quitGame()
+    {
+        Application.Quit();
+    }
+
+}
